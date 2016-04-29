b38740a3504e0443f6488910e09c8ade58467856
h4hany <hanysayed.a@gmail.com>
2016-04-29 15:24:03 +0300

    reg and edit and custom

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160428160229) do
+ActiveRecord::Schema.define(version: 20160429110427) do
 
   create_table "friends", force: :cascade do |t|
     t.integer  "fid",        limit: 4
@@ -91,6 +91,8 @@ ActiveRecord::Schema.define(version: 20160428160229) do
     t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
+    t.string   "name",                   limit: 255
+    t.string   "image",                  limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
