eb1ddc749088f9950de9d86049f189c3de57b895
h4hany <hanysayed.a@gmail.com>
2016-04-30 12:21:26 +0200

    upload image done

--- app/models/ordr.rb
+++ app/models/ordr.rb
@@ -1,4 +1,5 @@
 class Ordr < ActiveRecord::Base
-	attr_accessor :virtual_attribute
+  mount_uploader :menu, AttachmentUploader # Tells rails to use this uploader for this model.
+  attr_accessor :virtual_attribute
   belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  mount_uploader :image, AttachmentUploader # Tells rails to use this uploader for this model.
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
